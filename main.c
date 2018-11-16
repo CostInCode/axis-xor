eae7ec632ec1927e36cf1a3f8e2e8969384da4e2
CostInCode <costmarullo@gmail.com>
2018-11-16 15:36:41 +0100

    hand in

--- main.c
+++ main.c
@@ -5,9 +5,11 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <syslog.h>
+#include <math.h>
+#include <stdio.h>
 #include <capture.h>
 
-#define MAX_SIZE 50
+
 #define MAX_CLIENTS 5
 #define MY_PORT 9002
 
@@ -21,22 +23,15 @@ int main()
 {
     pthread_t connection_thread;
 
-    // CONNECT THE SERVER
-   
+    srand(time(NULL));
 
-    // create server socket tcp
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-    // define the server address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(MY_PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    // bind the socket to our specified IP and port
     bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-    // listen for connections
     listen(server_socket, MAX_CLIENTS);
 
     // for each client connecting to server, create a thread
@@ -44,68 +39,98 @@ int main()
         if((pthread_create(&connection_thread, NULL, connection_handler, NULL)) > 0)
             return 1;
     }
-    //pthread_exit(0);
     return 0;
 }
 
 
 void *connection_handler(void *mythread) {
 
-    char client_message[2000];
-    char specifications[2000];
-    
-        
-    int read_size;
+
+    /************ ENCRYPTION ********************/
+    // get PUBLIC KEY from client
+    char modulus[100];
+    recv(client_socket, modulus, 100, 0);
+
+    char *mod;
+    double modulusDouble;
    
-        
-    // while server keeps receiving message from client
-    while((read_size= recv(client_socket, client_message , 2000 , 0)) > 0) 
-    {
+    modulusDouble =  strtod(modulus, &mod);
 
-        sprintf(specifications, "%s", client_message);
-        media_frame  *frame;
-        media_stream *stream;
-        void *data;
-        size_t size;
-        stream = capture_open_stream(IMAGE_JPEG, specifications);
-        syslog(LOG_INFO, specifications);
+    char exponent[10];
+    char *exp;
+    recv(client_socket, exponent, 10, 0);
+   
+    double public_exponent;
+    public_exponent = strtod(exponent, &exp);
+    
 
-        // get size and data
+    // create xor key as a random number of 3 digits
+    int xor_key = rand() % 300 + 100;
+
+    syslog(LOG_INFO, "XORRR: %d\n", xor_key);
+    double key = (double) xor_key;
+
+    // encrypt rsa/xor_key and sends it
+    long double power_res = pow(key, public_exponent);
+    double result = fmod(power_res, modulusDouble);
+    
+    int encryptedXorKey = (int) result;
+    char encodedKeyString[10];
+    sprintf(encodedKeyString, "%d\n", encryptedXorKey);
+
+    write(client_socket, encodedKeyString, sizeof(encodedKeyString));
+
+    /************ IMAGE ********************/
+
+    char client_message[30];
+    recv(client_socket, client_message , 26, 0);
+
+    char decrypted[26];
+
+   /* int i;
+    for(i = 0; i < 27; i++) {
+        decrypted[i] = client_message[i] ^ encodedKeyString[i % sizeof(encodedKeyString)];
+    }*/
+    
+    media_frame  *frame;
+    media_stream *stream;
+    void *data;
+    size_t size;
+
+    stream = capture_open_stream(IMAGE_JPEG, client_message);
+
+    for(;;) {
+
         frame = capture_get_frame(stream);
         size = capture_frame_size(frame);
         data = capture_frame_data(frame);
 
+        if(size == NULL) return;
+
+        char *sizefh[100];
+        sprintf(sizefh,"%zu\n", size);
+   
+        size_t sizemg = size;
+
         char *mysize[100];
-        // format the size to be sent
+        size ^= xor_key;
         sprintf(mysize,"%zu\n", size);
-        syslog(LOG_INFO, mysize);
-        // send size to client
         write(client_socket, mysize, sizeof(mysize));
-
-        // prepare data array[size]
+        
+        /************************** IMAGE SENDING ****************************/
         int row = 0;
-        unsigned char rowData[size];
-        for(row = 0; row < size; row++) {
+        unsigned char rowData[sizemg];
+        for(row = 0; row < sizemg; row++) {
             rowData[row] = ((unsigned char *)data)[row];
+            rowData[row] ^= xor_key;
         }
+        if(rowData == NULL) return;
 
-        // send photo to client
-        int ret = write(client_socket, rowData, sizeof(rowData));
-        syslog(LOG_INFO, "SENT = %d",ret);
-       
-    
-        capture_frame_free(frame);
-        capture_close_stream(stream); 
+        write(client_socket, rowData, sizeof(rowData));
+        //syslog(LOG_INFO, "SENT IMAGE");
+        capture_frame_free(frame); 
     }
     
-    
-
-    
-    // close the server
-    //close(client_socket);
-    //pthread_exit(0);
-   
+    capture_close_stream(stream); 
+    close(client_socket);
 }
-
-
-
